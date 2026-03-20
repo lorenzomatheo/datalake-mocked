05abb1009e3ebe9d5a5f2400479a9cd922d5753c
Lorenzo <lorenzo.matheo@hotmail.com>
2026-03-20 08:22:50 -0300

    consertando formatacao

--- maggulake/utils/spark_udfs.py
+++ maggulake/utils/spark_udfs.py
@@ -1,9 +1,9 @@
-        from pyspark.sql import functions as F
-        from pyspark.sql import types as T
+from pyspark.sql import functions as F
+from pyspark.sql import types as T
 
-        from maggulake.utils.numbers import calculate_mode
-        from maggulake.utils.strings import converte_array_para_string
+from maggulake.utils.numbers import calculate_mode
+from maggulake.utils.strings import converte_array_para_string
 
 mode_udf = F.udf(calculate_mode, T.IntegerType())
 
-    array_to_string_udf = F.udf(converte_array_para_string, T.StringType())
+array_to_string_udf = F.udf(converte_array_para_string, T.StringType())
